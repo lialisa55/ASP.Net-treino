0a2bd6ceba523803493b6f7746c5cfa1c8ef9042
lialisa55 <nathalia.mota@etec.sp.gov.br>
2023-02-19 16:45:54 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
